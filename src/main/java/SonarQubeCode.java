1e92ada13f2d2f41db2eadabc8a59002cf3d7e5f
2503b05203 <2503b05203@sru.edu.in>
2026-01-27 23:18:22 +0530

    test java

--- src/main/java/SonarQubeCode.java
+++ src/main/java/SonarQubeCode.java
@@ -1,3 +1,10 @@
 public class SonarQubeCode {
-    print("");
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        SonarQubeCode app = new SonarQubeCode();
+        System.out.println("2 + 3 = " + app.add(2, 3));
+    }
 }
