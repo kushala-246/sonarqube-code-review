449fd9b0b233155f73fde4dc714c4952d816bf35
2503b05203 <2503b05203@sru.edu.in>
2026-01-28 07:38:12 +0530

    test java

--- src/main/java/com/ibm/SonarQubeCode.java
+++ src/main/java/com/ibm/SonarQubeCode.java
@@ -1,12 +1,7 @@
 package com.ibm;
 
 public class SonarQubeCode {
-    public int add(int a, int b) {
-        return a + b;
-    }
-
     public static void main(String[] args) {
-        SonarQubeCode app = new SonarQubeCode();
-        System.out.println("2 + 3 = " + app.add(2, 3));
+        System.out.println("SonarCloud Java test");
     }
 }
