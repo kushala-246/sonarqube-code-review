892b26dfad93b8757b582aadfe5006fd0b92fac5
Kushala R <kushala.ramdas@ibm.com>
2026-01-29 14:33:58 +0530

    Resolve merge conflict in test.java

---
+++ src/main/java/com/ibm/SonarQubeCode.java
@@ -0,0 +1,18 @@
+package com.ibm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SonarQubeCode {
+    private static final Logger logger = LoggerFactory.getLogger(SonarQubeCode.class);
+    public static final String MESSAGE = "SonarCloud Java test";
+
+
+    public static String getMessage() {
+        return MESSAGE
+    }
+
+    public static void main(String[] args) {
+        logger.info(getMessage());
+    }
+}
